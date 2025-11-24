e21ae5c63184d40764fb99c5ceab58a75c99f4d2
Эвелина Петрова <evvpetrova@mai.education>
2025-11-24 14:07:27 +0300

    end commit

--- src/data_structures.py
+++ src/data_structures.py
@@ -1,24 +1,19 @@
 class Stack:
     def __init__(self) -> None:
         self._items: list = []
-
     def push(self, x) -> None:
         self._items.append(x)
-
     def pop(self):
         if self.is_empty():
             raise ValueError("Stack is empty")
         return self._items.pop()
-
     def peek(self):
         if self.is_empty():
             raise ValueError("Empty stack")
         else:
             return self._items[-1]
-
     def is_empty(self) -> bool:
         return len(self._items) == 0
-
     def __len__(self) -> int:
         return len(self._items)
 
@@ -26,23 +21,18 @@ class Stack:
 class Queue:
     def __init__(self) -> None:
         self._items: list = []
-
     def enqueue(self, x) -> None:
         self._items.append(x)
-
     def dequeue(self):
         if self.is_empty():
             raise ValueError("Empty queue")
         else:
             return self._items.pop(0)
-
     def front(self):
         if self.is_empty():
             raise ValueError("Empty queue")
         return self._items[0]
-
     def is_empty(self) -> bool:
         return len(self._items) == 0
-
     def __len__(self) -> int:
         return len(self._items)
